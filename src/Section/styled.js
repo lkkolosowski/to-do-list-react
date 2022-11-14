59191baef55b4877dd8dc4ef1ffe774803231d52
Lukasz Kolosowski <lkkolosowski@gmail.com>
2022-11-14 06:52:30 +0100

    added styled.js for <Section> component

--- src/Section/styled.js
+++ src/Section/styled.js
@@ -1,23 +1,25 @@
-.section {
+import styled from "styled-components";
+
+export const Wrapper = styled.section`
   background-color: #fff;
   box-shadow: 0 1px 2px 0 rgb(34 34 34 / 5%);
   margin-bottom: 10px;
-}
+`;
 
-.section__header {
+export const Header = styled.header`
   display: flex;
   justify-content: space-between;
   border-bottom: 1px solid #dedede;
   padding: 20px;
-}
 
-@media screen and (max-width: 768px) {
-  .section__header {
+  @media (max-width: 768px) {
     flex-direction: column;
   }
-}
+`;
 
-.section__title {
+export const Title = styled.h2`
   font-size: 20px;
   margin: 0;
-}
+`;
+
+export const Body = styled.div([]);
