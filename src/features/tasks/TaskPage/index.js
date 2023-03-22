89905e52dc89e56abfd9636d8ef0f902029e4106
Lukasz Kolosowski <lkkolosowski@gmail.com>
2023-03-22 13:53:27 +0100

    add a condition that prevents errors

--- src/features/tasks/TaskPage/index.js
+++ src/features/tasks/TaskPage/index.js
@@ -7,12 +7,21 @@ import { getTaskById } from "../tasksSlice";
 
 function TaskPage() {
   const { id } = useParams();
-  const task = useSelector(state => getTaskById(state, id))
+  const task = useSelector((state) => getTaskById(state, id));
 
   return (
     <Container>
       <Header title="Szczegóły zadania" />
-      <Section title={task ? task.content : "Nie znaleziono zadania"} body={<><strong>Ukończono:</strong> {task.done ? "Tak" : "Nie"}</>} />
+      <Section
+        title={task ? task.content : "Nie znaleziono zadania"}
+        body={
+          !!task && (
+            <>
+              <strong>Ukończono:</strong> {task.done ? "Tak" : "Nie"}
+            </>
+          )
+        }
+      />
     </Container>
   );
 }
