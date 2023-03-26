5ddab40c90c2443974c672e66d5acf6524496854
Lukasz Kolosowski <lkkolosowski@gmail.com>
2023-03-27 01:33:17 +0200

    create Footer

--- src/features/author/AuthorPage/index.js
+++ src/features/author/AuthorPage/index.js
@@ -1,4 +1,5 @@
 import Container from "../../../common/Container";
+import Footer from "../../../common/Footer";
 import Header from "../../../common/Header";
 import Section from "../../../common/Section";
 import image from "../../../images/image.jpg";
@@ -6,36 +7,35 @@ import { ImageWrapper, Image, Paragraph } from "./styled";
 
 function AuthorPage() {
   return (
-    <Container>
-      <Header title="O autorze" />
-      <Section
-        title="Łukasz Kołosowski"
-        body={
-          <>
-            <ImageWrapper>
-              <Image
-                avatar
-                src={image}
-                alt="Łukasz"
-              />
-            </ImageWrapper>
-            <Paragraph>
-              Cześć! Nazywam się Łukasz, jestem z Wrocławia.
-            </Paragraph>
-            <Paragraph>
-              Jeżdżę dużo na rowerze. Lubię podziwiać widoczki (góry, lasy,
-              morze). Interesuję się programowaniem, muzyką{" "}
-              <strong>*</strong> oraz lubię sobie też czasem na siłkę
-              pochodzić.
-            </Paragraph>
-            <Paragraph>
-              <strong>*</strong> Ostatnio lubię suchać: <br />
-              Techno⚡Deep House⚡Future House⚡DnB
-            </Paragraph>
-          </>
-        }
-      />
-    </Container>
+    <>
+      <Container>
+        <Header title="O autorze" />
+        <Section
+          title="Łukasz Kołosowski"
+          body={
+            <>
+              <ImageWrapper>
+                <Image avatar src={image} alt="Łukasz" />
+              </ImageWrapper>
+              <Paragraph>
+                Cześć! Nazywam się Łukasz, jestem z Wrocławia.
+              </Paragraph>
+              <Paragraph>
+                Jeżdżę dużo na rowerze. Lubię podziwiać widoczki (góry,
+                lasy, morze). Interesuję się programowaniem, muzyką{" "}
+                <strong>*</strong> oraz lubię sobie też czasem na siłkę
+                pochodzić.
+              </Paragraph>
+              <Paragraph>
+                <strong>*</strong> Ostatnio lubię suchać: <br />
+                Techno⚡Deep House⚡Future House⚡DnB
+              </Paragraph>
+            </>
+          }
+        />
+      </Container>
+      <Footer />
+    </>
   );
 }
 
