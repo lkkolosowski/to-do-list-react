17fd4285f23a343e858f5f50bcfb3ebf930b9d01
Lukasz Kolosowski <lkkolosowski@gmail.com>
2023-03-27 00:18:43 +0200

    add some breakpoints and props to Container

--- src/common/Container/index.js
+++ src/common/Container/index.js
@@ -1,7 +1,7 @@
 import { Main } from "./styled";
 
-const Container = ({ children }) => (
-  <Main>{children}</Main>
+const Container = ({ children, trimmed }) => (
+  <Main trimmed={trimmed}>{children}</Main>
 );
 
 export default Container;
--- src/common/Container/styled.js
+++ src/common/Container/styled.js
@@ -1,7 +1,21 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Main = styled.main`
   max-width: 900px;
   margin: 0 auto;
   padding: 20px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    padding: 0 10px;
+  }
+
+  ${({ trimmed }) =>
+    trimmed &&
+    css`
+      padding: 0 20px;
+
+      @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        padding: 0;
+      }
+    `};
 `;
