893233ee408a299ac55f7478d3efccf6ff77e746
Lukasz Kolosowski <lkkolosowski@gmail.com>
2023-03-27 01:50:28 +0200

    small adjustment

--- src/common/Container/styled.js
+++ src/common/Container/styled.js
@@ -3,16 +3,12 @@ import styled, { css } from "styled-components";
 export const Main = styled.main`
   max-width: 900px;
   margin: 0 auto;
-  padding-top: 20px;
-
-  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-    padding: 20px 10px 0;
-  }
+  padding: 20px 10px 0;
 
   ${({ trimmed }) =>
     trimmed &&
     css`
-      padding: 0 20px;
+      padding: 0 10px;
 
       @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
         padding: 0;
