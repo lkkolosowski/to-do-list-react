da956f103fec3568a59bb7cded38bc724fbf3997
lkkolosowski <lkkolosowski@gmail.com>
2023-06-17 04:15:12 +0200

    change max width of page

--- src/common/Footer/styled.js
+++ src/common/Footer/styled.js
@@ -1,7 +1,14 @@
 import styled from "styled-components";
+import { ReactComponent as Heart } from "../../images/heart.svg";
 
 export const StyledFooter = styled.footer`
-  max-width: 900px;
+  max-width: 1400px;
   margin: 0 auto;
   text-align: center;
+`;
+
+export const StyledHeart = styled(Heart)`
+  height: 16px;
+  width: auto;
+  margin-bottom: -3px;
 `;
